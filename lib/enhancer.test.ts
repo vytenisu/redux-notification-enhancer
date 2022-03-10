a8536cb69eaef39d703db79199199160ce0b26cc
Vytenis <wyt.dislike@gmail.com>
2022-03-10 17:47:47 +0200

    Preparation for tests

---
+++ lib/enhancer.test.ts
@@ -0,0 +1,43 @@
+import {createNotificationEnhancer} from './enhancer'
+
+describe('Redux Notification Enhancer', () => {
+  it('skips render when passive action is called', () => {
+    // const {} = createNotificationEnhancer({})
+  })
+
+  it('does not skip render when passive flag is not used', () => {
+    // TODO: NOT FINISHED
+  })
+
+  it('throttles render when throttle is enabled', () => {
+    // TODO: NOT FINISHED
+  })
+
+  it('does not throttle when throttle is disabled', () => {
+    // TODO: NOT FINISHED
+  })
+
+  it('triggers animation frame when triggering is enabled', () => {
+    // TODO: NOT FINISHED
+  })
+
+  it('does not trigger animation frame when triggering is disabled', () => {
+    // TODO: NOT FINISHED
+  })
+
+  it('allows modifying passive action type prefix', () => {
+    // TODO: NOT FINISHED
+  })
+
+  it('allows modifying immediate action type prefix', () => {
+    // TODO: NOT FINISHED
+  })
+
+  it('triggers immediate action at once', () => {
+    // TODO: NOT FINISHED
+  })
+
+  it('notifies when render is complete', () => {
+    // TODO: NOT FINISHED
+  })
+})
